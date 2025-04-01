79d09fe037603db1ede156b2ad0af2aa20b9b361
Aman Sharma <amansha2047@gmail.com>
2025-04-01 01:54:07 -0400

    Created getUsers and getUserById API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,6 +7,34 @@ const bcrypt = require("bcryptjs");
 // @access Private/Admin
 const getUsers = async (req, res) => {
   try {
+    const users = await User.find({}).select("-password");
+
+    // Add tasl count to each user
+    const usersWithTaskCount = await Promise.all(
+      users.map(async (user) => {
+        const pendingTasks = await Task.countDocuments({
+          assignedTo: user._id,
+          status: "pending",
+        });
+        const inProgressTasks = await Task.countDocuments({
+          assignedTo: user._id,
+          status: "in-progress",
+        });
+        const completedTasks = await Task.countDocuments({
+          assignedTo: user._id,
+          status: "completed",
+        });
+
+        return {
+          ...user._doc,
+          pendingTasks,
+          inProgressTasks,
+          completedTasks,
+        };
+      })
+    );
+
+    res.status(200).json(usersWithTaskCount);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
@@ -17,6 +45,11 @@ const getUsers = async (req, res) => {
 // @access Private/Admin
 const getUserById = async (req, res) => {
   try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
