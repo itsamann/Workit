80593a138c2a76e4479a0b63deab9cb57f6cc1e8
Aman Sharma <amansha2047@gmail.com>
2025-04-05 15:56:36 -0400

    Get Task By ID API

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -76,6 +76,15 @@ const getTasks = async (req, res) => {
 // @access  Private
 const getTaskById = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profilePic"
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
