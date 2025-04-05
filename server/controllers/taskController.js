6a9cfcf011a66025b00c21e7e51b0200b3c36059
Aman Sharma <amansha2047@gmail.com>
2025-04-05 16:08:48 -0400

    Update Task API

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -142,6 +142,28 @@ const createTask = async (req, res) => {
 // @access  Private
 const updateTask = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority;
+    task.dueDate = req.body.dueDate || task.dueDate;
+    task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+    task.attachments = req.body.attachments || task.attachments;
+
+    if (req.body.assignedTo) {
+      if (!Array.isArray(req.body.assignedTo)) {
+        return res
+          .status(400)
+          .json({ message: "AssignedTo must be an array of user IDs" });
+      }
+      task.assignedTo = req.body.assignedTo;
+    }
+    const updatedTask = await task.save();
+    res.status(200).json({ message: "Task updated successfully", updatedTask });
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
