ae495a5f8ba85ede72dd433bf6040efa25d05b3b
Aman Sharma <amansha2047@gmail.com>
2025-05-31 03:49:27 -0400

    Update Task Done

--- client/src/pages/Admin/CreateTask.jsx
+++ client/src/pages/Admin/CreateTask.jsx
@@ -80,8 +80,35 @@ const CreateTask = () => {
 
   // update task
   const updateTask = async () => {
-    // TODO: implement update logic
-    toast("Update task logic not implemented");
+    setLoading(true);
+
+    try {
+      const todolist = taskData.todoChecklist?.map((item) => {
+        const prevTodoChecklist = currentTask?.todoChecklist || [];
+        const matchedTask = prevTodoChecklist.find((task) => task.text == item);
+
+        return {
+          text: item,
+          completed: matchedTask ? matchedTask.completed : false,
+        };
+      });
+
+      const response = await axiosInstance.put(
+        API_PATHS.TASKS.UPDATE_TASK(taskId),
+        {
+          ...taskData,
+          dueDate: new Date(taskData.dueDate).toISOString(),
+          todoChecklist: todolist,
+        }
+      );
+
+      toast.success("Task Updated Successfully");
+    } catch (error) {
+      console.error("Error updating task:", error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const handleSubmit = async () => {
