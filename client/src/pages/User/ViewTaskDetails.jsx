0759421969c870d7452662ce39944d4d5ca30f7e
Aman Sharma <amansha2047@gmail.com>
2025-06-01 05:08:50 -0400

    API call for ViewTaskDetails from client side

--- client/src/pages/User/ViewTaskDetails.jsx
+++ client/src/pages/User/ViewTaskDetails.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosInstance";
 
 const ViewTaskDetails = () => {
+  const { id } = useParams();
+  const [task, setTask] = useState(null);
+
+  const getStatusTagColor = (status) => {
+    switch (status) {
+      case "In Progress":
+        return "text-cyan-500 bg-cyan-50 border border-cyan-500/10";
+
+      case "Completed":
+        return "text-lime-500 bg-lime-50 border border-lime-500/20";
+
+      default:
+        return "text-violet-500 bg-violet-50 border border-violet-500/10";
+    }
+  };
+
+  // get Task info by ID
+  const getTaskDetailsByID = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_TASK_BY_ID(id)
+      );
+
+      if (response) {
+        const taskInfo = response.data;
+        setTask(taskInfo);
+      }
+    } catch (error) {
+      console.error("Error Fetching Task:", error);
+    }
+  };
+
+  // handle todo check
+  const updateTodoChecklist = async (index) => {};
+
+  // Handle attachment link click
+  const handleLinkClick = (link) => {
+    window.open(link, "_blank");
+  };
+
+  useEffect(() => {
+    if (id) {
+      getTaskDetailsByID();
+    }
+    return () => {};
+  }, [id]);
+
   return <div>ViewTaskDetails</div>;
 };
 
